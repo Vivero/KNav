6c9a4e64886141a2e4917b62b3967d0a421aa72e
Vivero <Vivero@users.noreply.github.com>
2015-04-06 00:40:15 -0400

    enhance cmd line args

--- KNav/src/main.cpp
+++ KNav/src/main.cpp
@@ -11,6 +11,13 @@ using namespace std;
 
 #define KNAV_SLEEP_PERIOD 80
 
+#define DEFAULT_KRPC_SERVER_IP           "127.0.0.1"
+#define DEFAULT_KRPC_SERVER_IP_LEN                9
+#define DEFAULT_KRPC_SERVER_PORT             "50000"
+#define DEFAULT_KRPC_SERVER_PORT_LEN              5
+#define DEFAULT_KRPC_STREAM_PORT             "50001"
+#define DEFAULT_KRPC_STREAM_PORT_LEN              5
+
 BOOL knavTerminate = FALSE;
 ULONGLONG systemTime_ms = 0, executionTime_ms = 0;
 LONGLONG  sleepPeriod = 0;
@@ -18,9 +25,17 @@ ULONG execCounts = 0;
 double avgExecTime = 0.0;
 HANDLE hStdout;
 
+char *krpc_ipAddress;
+char *krpc_rpcPort;
+char *krpc_streamPort;
+
+HRESULT ParseCmdLine(int &argc, _TCHAR* argv[]);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+  // process command line args
+  ParseCmdLine(argc, argv);
+
   KRPCI& krpcInterface = KRPCI::getInstance();
   
   // setup Telemetry capture
@@ -40,20 +55,10 @@ int _tmain(int argc, _TCHAR* argv[])
   BOOL userInputLocked = FALSE;
 
   cout << endl << "Boot up sequence ... " << endl;
-
-  // get IP address from cmd line
-  const int ipAddrBufSize = 16;
-  char krpcIpAddress[ipAddrBufSize]; ZeroMemory(krpcIpAddress, ipAddrBufSize);
-  if (argc > 1) {
-    _snprintf_s(krpcIpAddress, ipAddrBufSize - 1, "%S", argv[1]);
-  }
-  else {
-    _snprintf_s(krpcIpAddress, ipAddrBufSize - 1, "127.0.0.1");
-  }
-  cout << "Connecting to " << krpcIpAddress << " ... " << endl;
+  cout << "Connecting to " << krpc_ipAddress << " ... " << endl;
 
   // open interface to KRPC
-  krpcInterface.Open(krpcIpAddress);
+  krpcInterface.Open(krpc_ipAddress);
   cout << "KRPC Version: " << krpcInterface.GetKRPCServerVersion() << endl << endl;
 
   // initialize KNav Telemetry
@@ -132,6 +137,61 @@ int _tmain(int argc, _TCHAR* argv[])
   // reset console
   SetConsoleTextAttribute(hStdout, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
 
+  delete krpc_ipAddress;
+  delete krpc_rpcPort;
+  delete krpc_streamPort;
+
   return 0;
 }
 
+HRESULT ParseCmdLine(int &argc, _TCHAR* argv[])
+{
+  // get command line arguments:
+  //   argv[0]     optional: ip address
+  //   argv[1]     optional: rpc server port
+  //   argv[2]     optional: stream server port
+  //
+  size_t numCharsConverted = 0;
+  size_t argNumChars = 0;
+  HRESULT retval = S_OK;
+
+  // parse IP address
+  if (argc > 1) {
+    argNumChars = wcslen(argv[1]);
+    krpc_ipAddress = new char[argNumChars + 1];
+    wcstombs_s(&numCharsConverted, krpc_ipAddress, argNumChars + 1, argv[1], _TRUNCATE);
+  }
+  else {
+    krpc_ipAddress = new char[DEFAULT_KRPC_SERVER_IP_LEN + 1];
+    _snprintf_s(krpc_ipAddress, DEFAULT_KRPC_SERVER_IP_LEN + 1, _TRUNCATE, DEFAULT_KRPC_SERVER_IP);
+  }
+
+  // parse RPC server port
+  if (argc > 2) {
+    argNumChars = wcslen(argv[2]);
+    krpc_rpcPort = new char[argNumChars + 1];
+    wcstombs_s(&numCharsConverted, krpc_rpcPort, argNumChars + 1, argv[2], _TRUNCATE);
+  }
+  else {
+    krpc_rpcPort = new char[DEFAULT_KRPC_SERVER_PORT_LEN + 1];
+    _snprintf_s(krpc_rpcPort, DEFAULT_KRPC_SERVER_PORT_LEN + 1, _TRUNCATE, DEFAULT_KRPC_SERVER_PORT);
+  }
+
+  // parse stream server port
+  if (argc > 3) {
+    argNumChars = wcslen(argv[3]);
+    krpc_streamPort = new char[argNumChars + 1];
+    wcstombs_s(&numCharsConverted, krpc_streamPort, argNumChars + 1, argv[3], _TRUNCATE);
+  }
+  else {
+    krpc_streamPort = new char[DEFAULT_KRPC_STREAM_PORT_LEN + 1];
+    _snprintf_s(krpc_streamPort, DEFAULT_KRPC_STREAM_PORT_LEN + 1, _TRUNCATE, DEFAULT_KRPC_STREAM_PORT);
+  }
+
+  // print out cmd line args
+  printf("kRPC Host IP Address      : %s \n", krpc_ipAddress);
+  printf("kRPC RPC Server Port      : %s \n", krpc_rpcPort);
+  printf("kRPC Stream Server Port   : %s \n", krpc_streamPort);
+
+  return retval;
+}
